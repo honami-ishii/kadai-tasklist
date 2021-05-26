8e0f4adc6e844841e7273befd95bd6e9f9f14157
honami.ishii <srarsxs236@gmail.com>
2021-05-26 12:58:46 +0000

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
     root to: 'tasks#index'
+    resources :tasks
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
   
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
@@ -9,7 +11,7 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :create]
   
-  resources :tasks, only: [:create, :destroy]
+  
 end
 
 
