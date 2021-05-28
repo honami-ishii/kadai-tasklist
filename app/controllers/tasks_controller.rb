d541bc4e69537c6adb049e10dad148437aa2f15b
honami.ishii <srarsxs236@gmail.com>
2021-05-28 04:49:04 +0000

    task controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,7 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy,:show,:edit,:update]
+  
   def index
       if logged_in?
       @task = current_user.tasks.build  # form_with 用
@@ -9,7 +10,7 @@ class TasksController < ApplicationController
   end
   
   def show
-      @user = User.find(params[:id])
+      @task = Task.find(params[:id])
   end
 
   def new
@@ -28,6 +29,10 @@ class TasksController < ApplicationController
       render :new
     end
   end
+  
+  def edit
+    @task = Task.find(params[:id])
+  end
 
   def update
       
@@ -44,15 +49,18 @@ class TasksController < ApplicationController
     @task.destroy
 
     flash[:success] = 'Task は正常に削除されました'
-    redirect_to tasks_url
+    redirect_back(fallback_location: root_path)
   end
   
     private
-    
-  def set_message
-    @task = Task.find(params[:id])
-  end
 
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
+    
   # Strong Parameter
   def task_params
     params.require(:task).permit(:content, :status)
